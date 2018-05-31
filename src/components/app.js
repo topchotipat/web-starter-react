fdcb6417a8c8898937b1237bf2f04e3d22fdd340
Chotipat Poowongthanarat <topz.classic@gmail.com>
2018-05-31 16:31:43 +0700

    Setup start project

---
+++ src/components/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    console.log('111111')
+    return (
+      <div>OK</div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  plugins: [  
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    port: 3090,
+    contentBase: path.join(__dirname, 'dist')
+  }
+};
