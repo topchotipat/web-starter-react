b86dc82118d0898a527199e2fd170b8b13a202de
Chotipat Poowongthanarat <topz.classic@gmail.com>
2018-06-07 16:10:16 +0700

    Error Page

--- src/route/index.js
+++ src/route/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Login from '../containers/Login'
+import ErrorPage from '../containers/ErrorPage'
 import Dashboard from '../containers/Dashboard'
 import App from '../app'
 import PrivateRoute from './PrivateRoute'
+import { connect } from 'react-redux'
 
-
-const routes = () =>(
+const routes = (props) =>(
     <BrowserRouter>
         <App>
             <Switch>
                 <Route path="/login" component={Login} />
-                <PrivateRoute exact path="/" component={Dashboard} />
+                <PrivateRoute exact path="/" component={Dashboard} isLoggedin={props.auth}/>
+                <Route path="*" component={ErrorPage} />
             </Switch>
         </App> 
     </BrowserRouter>
 )
 
-export default routes
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps)(routes)
