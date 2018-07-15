63035ec9567c4b76194682236b1b576a3dddb1b5
Chotipat Poowongthanarat <topz.classic@gmail.com>
2018-07-15 19:08:03 +0700

    hot fix private route

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,4 +1,13 @@
-export const isEmail = (email) =>{
+export const isEmail = (email) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
+}
+
+export const isEmpty = (value) => {
+    if (value === undefined || value === null ||
+        (typeof value === 'object' && Object.keys(value).length === 0) ||
+        (typeof value === 'string' && value.trim().length === 0)) {
+        return true
+    }
+    return false
 }
